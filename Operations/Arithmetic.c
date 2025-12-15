3c30f44e94e8dec1dadecaf30ce78557c7163687
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-15 22:56:04 +0530

    Create Arithmetic Operations

---
+++ Operations/Arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	float n1, n2;
+	printf("Enter Number-1:");
+	scanf("%f",&n1);
+	printf("Enter Number-2:");
+	scanf("%f",&n2);
+	
+	printf("%.2f + %.2f = %.2f\n",n1,n2,n1+n2);
+	printf("%.2f - %.2f = %.2f\n",n1,n2,n1-n2);
+	printf("%.2f * %.2f = %.2f\n",n1,n2,n1*n2);
+	printf("%.2f / %.2f = %.2f\n",n1,n2,n1/n2);
+}
