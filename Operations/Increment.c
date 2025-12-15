2922432a5b9eaca7254591e862075d1f130c5860
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-15 22:59:52 +0530

    Create Increment Operations

---
+++ Operations/Increment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int i;
+	printf("Enter any integer:");
+	scanf("%d",&i);
+	
+	//Increment	
+	printf("\n%d",i);
+	printf("\n%d",i++);
+	printf("\n%d",++i);
+	
+	//Decrement
+	printf("\n%d",i);
+	printf("\n%d",i--);
+	printf("\n%d",--i);
+}
