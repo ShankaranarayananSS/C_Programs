ddaf69dfce72cb9942312615424dbd13db3b01eb
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-15 22:57:35 +0530

    Create Relational Operations

---
+++ Operations/Relational.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	
+	float n1,n2;
+	printf("Enter two numbers:");
+	scanf("%f",&n1);
+	scanf("%f",&n2);
+		
+	printf("\n%.2f < %.2f : %d",n1,n2,n1<n2);
+	printf("\n%.2f > %.2f : %d",n1,n2,n1>n2);
+	printf("\n%.2f <= %.2f : %d",n1,n2,n1<=n2);
+	printf("\n%.2f <= %.2f : %d",n1,n2,n1<=n2);
+	printf("\n%.2f == %.2f : %d",n1,n2,n1==n1);
+	printf("\n%.2f != %.2f : %d",n1,n2,n1!=n1);
+}
