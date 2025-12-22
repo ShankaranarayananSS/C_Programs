7ce843efe67c4d0dffe51ff0002ed67e9f1a4b8e
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-22 17:51:31 +0530

    Create Numbers

---
+++ Simple Programs/Numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int num;
+	printf("Enter any number:");
+	scanf("%d",&num);
+	printf("\nSquare of Number: %d",num*num);
+	printf("\nCube of Number: %d",num*num*num);
+	return 0;
+}
