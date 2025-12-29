79437e4b6866f5cfb361343c043c5930cb6ba299
SHANKARANARAYANAN S S <usssn43@gmail.com>
2025-12-29 22:09:01 +0530

    Create Leap Year

---
+++ Conditional Statement/Leap Year.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int year;
+	printf("Enter any year:");
+	scanf("%d",&year);
+	if(year%4==0){
+		printf("%d is Leap year!",year);
+	}
+	else{
+		printf("%d is not a Leap year",year);
+	}
+	return 0;
+}
