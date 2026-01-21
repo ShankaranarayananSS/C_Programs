9701a8de3ad6887eb6a3cdd358c325bb2cb61d3e
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-21 21:45:08 +0530

    Update Arithmetic Operations

--- Operations/Arithmetic.c
+++ Operations/Arithmetic.c
@@ -11,4 +11,9 @@ int main(){
 	printf("%.2f - %.2f = %.2f\n",n1,n2,n1-n2);
 	printf("%.2f * %.2f = %.2f\n",n1,n2,n1*n2);
 	printf("%.2f / %.2f = %.2f\n",n1,n2,n1/n2);
+	
+	int num1 = n1;
+	int num2 = n2;
+	int mod = num1%num2;
+	printf("%.2f %% %.2f = %d\n",n1,n2,mod);
 }
