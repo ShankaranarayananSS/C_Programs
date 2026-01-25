65512de0992049d5667634b1c9adb7f7ef094617
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-01-25 09:37:35 +0530

    Create Factorial For Loop

---
+++ Looping Statement/For Loop/Factorial.c
@@ -0,0 +1,14 @@
+//For Loop
+//Factorial
+
+#include<stdio.h>
+int main(){
+  int i,n,fact=1;
+  printf("Enter any number:");
+  scanf("%d",&n);
+  for(i=n;i>=1;i--){
+    fact = fact*i;
+  }
+  printf("Factorial of %d is %d",n,fact);
+  return 0;
+}
