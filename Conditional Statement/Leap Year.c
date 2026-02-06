7256ae00e0fca97f7de010a4be46cdcfb3a72574
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-06 22:46:43 +0530

    Update Leap Year

--- Conditional Statement/Leap Year.c
+++ Conditional Statement/Leap Year.c
@@ -3,7 +3,7 @@ int main(){
 	int year;
 	printf("Enter any year:");
 	scanf("%d",&year);
-	if(year%4==0){
+	if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
 		printf("%d is Leap year!",year);
 	}
 	else{
