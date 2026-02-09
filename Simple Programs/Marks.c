3c0055f391811570acc915f588d3178b5c21c8f5
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-09 19:09:00 +0530

    Update Marks

--- Simple Programs/Marks.c
+++ Simple Programs/Marks.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
+
 int main(){
-	int m1,m2,m3,m4,m5,total;
-	float avg;
+	float m1,m2,m3,m4,m5,total,avg;
 	printf("Enter mark-1:");
-	scanf("%d",&m1);
+	scanf("%f",&m1);
 	printf("Enter mark-2:");
-	scanf("%d",&m2);
+	scanf("%f",&m2);
 	printf("Enter mark-3:");
-	scanf("%d",&m3);
+	scanf("%f",&m3);
 	printf("Enter mark-4:");
-	scanf("%d",&m4);
+	scanf("%f",&m4);
 	printf("Enter mark-5:");
-	scanf("%d",&m5);
+	scanf("%f",&m5);
 	total = m1+m2+m3+m4+m5;
 	avg = total/5;
-	printf("\nTotal Marks: %d",total);
+	printf("\nTotal Marks: %.2f",total);
 	printf("\nAverage Marks: %.2f",avg);
 	return 0;
 }
