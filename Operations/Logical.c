bbf3624089c6cd6f249b87fcfed3e78d84a40987
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-02-27 08:42:45 +0530

    Update Logical Operations

--- Operations/Logical.c
+++ Operations/Logical.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 int main(){
-  
-  int t=1, f=0;
-  printf("Truth Tables:");
-  
-  printf("\n\nAND OPERATOR:");
-  printf("\n%d && %d = %d",t, t, t&&t);
-  printf("\n%d && %d = %d",f, t, f&&t);
-  printf("\n%d && %d = %d",t, f, t&&f);
-  printf("\n%d && %d = %d",f, f, f&&f);
-  
-  printf("\n\nOROPERATOR:");
-  printf("\n%d || %d = %d",t, t, t||t);
-  printf("\n%d || %d = %d",f, t, f||t);
-  printf("\n%d || %d = %d",t, f, t||f);
-  printf("\n%d || %d = %d",f, f, f||f);
-  
-  printf("\n\nNOTOPERATOR:");
-  printf("\n!%d = %d",t, !t);
-  printf("\n!%d = %d",f, !f);
+	int t=1, f=0;
+	printf("Truth Tables:");
+	
+	printf("\n\nAND OPERATOR:");
+	printf("\n%d && %d = %d",t, t, t&&t);
+	printf("\n%d && %d = %d",f, t, f&&t);
+	printf("\n%d && %d = %d",t, f, t&&f);
+	printf("\n%d && %d = %d",f, f, f&&f);
+	
+	printf("\n\nOROPERATOR:");
+	printf("\n%d || %d = %d",t, t, t||t);
+	printf("\n%d || %d = %d",f, t, f||t);
+	printf("\n%d || %d = %d",t, f, t||f);
+	printf("\n%d || %d = %d",f, f, f||f);
+	
+	printf("\n\nNOTOPERATOR:");
+	printf("\n!%d = %d",t, !t);
+	printf("\n!%d = %d",f, !f);
 	return 0;
 }
