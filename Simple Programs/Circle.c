0d4dc0677910964b97467ca37c5ace759f43a888
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-03-04 08:53:23 +0530

    Update Circle

--- Simple Programs/Circle.c
+++ Simple Programs/Circle.c
@@ -1,11 +1,12 @@
-//	Circumference of Circle	: 2 * Pi * Radius
-//	Area of Circle		: Pi * Radius * Radius
+//Circumference of Circle	: 2 * Pi * Radius
+//Area of Circle			: Pi * Radius * Radius
 
 #include<stdio.h>
 int main(){
 	float r;
 	printf("Enter radius of a Circle:");
 	scanf("%f",&r);
+	
 	printf("\nCircumference of Circle: %.2f",2*3.14*r);
 	printf("\nArea of Circle: %.2f",3.14*r*r);
 	return 0;
