c4dafe6b8a6b087c46e6631750e217d4fd994566
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-03-04 22:30:33 +0530

    Update Rectangle

--- Simple Programs/Rectangle.c
+++ Simple Programs/Rectangle.c
@@ -1,6 +1,6 @@
-//	Perimeter of Rectangle	: 2 * (Length + Breadth)
-//	Area of Rectangle		: Length * Breadth
-	
+//Perimeter of Rectangle	: 2 * (Length + Breadth)
+//Area of Rectangle			: Length * Breadth
+
 #include<stdio.h>
 int main(){
 	float l,b;
@@ -8,6 +8,7 @@ int main(){
 	scanf("%f",&l);
 	printf("Enter Breadth of Rectangle:");
 	scanf("%f",&b);
+	
 	printf("\nPerimeter of Rectangle: %.2f",2*(l+b));
 	printf("\nArea of Rectangle: %.2f",l*b);
 	return 0;
