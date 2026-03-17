17873a50937a7202b1b96a239e439a4d6c187f8f
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-03-17 23:02:34 +0530

    Update Temperature Conversion Program 6

--- Simple Programs/Temperature6.c
+++ Simple Programs/Temperature6.c
@@ -3,11 +3,11 @@
 //K = (F-32) * 5/9 + 273.15
 
 #include<stdio.h>
-
 int main(){
   float f;
   printf("Enter Fahrenheit:");
   scanf("%f",&f);
+  
   printf("Kelvin = %.2f",(f-32)*5/9+273.15);
   return 0;
 }
