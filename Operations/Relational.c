4d1e6802c686c795c1fcceff70d81f939967abd8
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-03-30 23:27:19 +0530

    Update Relational Operations

--- Operations/Relational.c
+++ Operations/Relational.c
@@ -2,8 +2,7 @@
 int main(){
 	float n1,n2;
 	printf("Enter two numbers:");
-	scanf("%f",&n1);
-	scanf("%f",&n2);
+	scanf("%f%f",&n1,&n2);
 	
 	printf("\n%.2f < %.2f : %d",n1,n2,n1<n2);
 	printf("\n%.2f > %.2f : %d",n1,n2,n1>n2);
