e990fd57f259da95f5b213325b70b4c9921a00e8
TAMILNADU COMPUTER SOFTWARE COLLEGE <272538512+TNCSC-TRICHY@users.noreply.github.com>
2026-04-01 17:21:18 +0530

    Update Factorial

--- Looping Statement/Do While/Factorial.c
+++ Looping Statement/Do While/Factorial.c
@@ -3,15 +3,16 @@
 
 #include<stdio.h>
 int main(){
-  int num,i=1,fact=1;
-  printf("Enter any number:");
-  scanf("%d",&num);
-  do{
-    fact = fact*i;
-    i++;
-  }
-  while(i<=num);{
-    printf("Factorial of %d is %d",num,fact);
+	int num,i=1,fact=1;
+	printf("Enter any number:");
+	scanf("%d",&num);
+	
+	do{
+		fact = fact*i;
+		i++;
 	}
-  return 0;
+	while(i<=num);{
+		printf("Factorial of %d is %d",num,fact);
+	}
+	return 0;
 }
