bae10fa43e0505f6f5fac4eb672290f7e92b6184
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-06-01 08:53:35 +0530

    Create Biggest of Two numbers

---
+++ Conditional Statement/If Else Ladder/Biggest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int n1,n2;
+  printf("Enter number-1:");
+  scanf("%d",&n1);
+  printf("Enter number-2:");
+  scanf("%d",&n2);
+  
+  if(n1>n2){
+    printf("%d is bigger than %d",n1,n2);
+  }else if(n1<n2){
+    printf("%d is bigger than %d",n2,n1);
+  }else{
+    printf("Both are same");
+  }
+  return 0;
+}
