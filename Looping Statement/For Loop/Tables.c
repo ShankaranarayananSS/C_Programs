078812c0c8bf9b06a8f28e514ce2881c06865356
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-06-11 23:45:05 +0530

    Create Multiplication Tables For Loop

---
+++ Looping Statement/For Loop/Tables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int num,i;
+  printf("Enter number:");
+  scanf("%d",&num);
+  
+  printf("%d Tables:",num);
+  for(i=1;i<=10;i++){
+    printf("\n%d * %d = %d",num,i,num*i);
+  }
+  return 0;
+}
