1cd890899a148325ebf2a565229c99e3cda23919
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-06-11 23:48:03 +0530

    Create Multiplication Tables While Loop

---
+++ Looping Statement/While Loop/Tables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int num,i=1;
+  printf("Enter number:");
+  scanf("%d",&num);
+  
+  printf("%d Tables:",num);
+  while(i<=10){
+    printf("\n%d * %d = %d",num,i++,num*i);
+  }
+  return 0;
+}
