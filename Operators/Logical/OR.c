5f2b0fb0107847b43eff13f7599e3370b0c947aa
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-17 09:04:35 +0530

    Update Logical OR

--- Operators/Logical/OR.c
+++ Operators/Logical/OR.c
@@ -2,9 +2,9 @@
 int main(){
   int f=0,t=1;
   printf("Logical OR");
-  printf("\n%d && %d = %d",f,f,(f||f));
-  printf("\n%d && %d = %d",f,t,(f||t));
-  printf("\n%d && %d = %d",t,f,(t||f));
-  printf("\n%d && %d = %d",t,t,(t||t));
+  printf("\n%d || %d = %d",f,f,(f||f));
+  printf("\n%d || %d = %d",f,t,(f||t));
+  printf("\n%d || %d = %d",t,f,(t||f));
+  printf("\n%d || %d = %d",t,t,(t||t));
   return 0;
 }
