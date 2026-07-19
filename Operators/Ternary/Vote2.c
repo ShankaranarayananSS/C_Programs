1ef997731a3f5bfe7bdf1016839745a4a231e40f
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-19 20:41:44 +0530

    Create Vote Eligibility Version 2

---
+++ Operators/Ternary/Vote2.c
@@ -0,0 +1,11 @@
+//Vote Eligibility - Version 2
+
+#include<stdio.h>
+int main(){
+  int age;
+  printf("Enter age:");
+  scanf("%d",&age);
+  
+  (age<18)?printf("Not eligible"):printf("Eligible");
+  return 0;
+}
