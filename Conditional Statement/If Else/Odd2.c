3cba17d97c9fa19c005729ebfa6d716dea884f6a
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-23 22:08:38 +0530

    Create Odd Or Even Version 2

---
+++ Conditional Statement/If Else/Odd2.c
@@ -0,0 +1,15 @@
+//Odd Or Even - Version 2
+
+#include<stdio.h>
+int main(){
+  int n;
+  printf("Enter number:");
+  scanf("%d",&n);
+  
+  if(n%2==1){
+    printf("Odd");
+  }else{
+    printf("Even");
+  }
+  return 0;
+}
