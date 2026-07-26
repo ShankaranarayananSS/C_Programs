67297dc3fe3cf4dcb5323594c28316b75ec2b241
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-07-26 09:27:20 +0530

    Create Days

---
+++ Conditional Statement/Switch/Days.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int day;
+  printf("Enter day number:");
+  scanf("%d",&day);
+  
+  switch(day){
+    case 1:printf("SUN");break;
+    case 2:printf("MON");break;
+    case 3:printf("TUE");break;
+    case 4:printf("WED");break;
+    case 5:printf("THU");break;
+    case 6:printf("FRI");break;
+    case 7:printf("SAT");break;
+    default:printf("No such Day");break;
+  }
+  return 0;
+}
