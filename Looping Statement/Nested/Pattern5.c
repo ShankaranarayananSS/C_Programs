9a3934d2eb94b56681dd4136d6cd5ae6a5576f9e
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-13 18:11:40 +0530

    Create Pattern

---
+++ Looping Statement/Nested/Pattern5.c
@@ -0,0 +1,19 @@
+/*
+EXPECTED PATTERN
+1
+2  3
+4  5  6
+*/
+
+#include<stdio.h>
+int main(){
+  int i,j,k=1;
+  for(i=1;i<=3;i++){
+    for(j=1;j<=i;j++){
+      printf("%d\t",k);
+      k++;
+    }
+    printf("\n");
+  }
+  return 0;
+}
