81d1b3f09359ade8910baab4af2db8ea67635e40
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-24 22:52:16 +0530

    Create Increment Operator

---
+++ Operators/Unary/Inc2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int i=10;
+  printf("Post Increment:");
+  
+  //Displays "i" value first, then increases it
+  printf("\nStep 1 = %d",i++);
+  printf("\nStep 2 = %d",i);
+  return 0;
+}
