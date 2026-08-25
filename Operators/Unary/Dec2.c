68b41b3b295d676097fdc0c0e6f134a5e752e10d
SHANKARANARAYANAN S S <usssn43@gmail.com>
2026-08-25 20:29:06 +0530

    Create Decrement Operator

---
+++ Operators/Unary/Dec2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int i=10;
+  printf("Post Decrement:");
+  
+  //Displays "i" value first, then decreases it
+  printf("\nStep 1 = %d",i--);
+  printf("\nStep 2 = %d",i);
+  return 0;
+}
